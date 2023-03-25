ed31951da4851212de3219cc8155c2e4817bfd28
lsl <loicsaintpaul@hotmail.fr>
2023-03-25 09:48:52 +0100

    [Security] Authenticate et Swagger accessible à tous, le reste demande une authentication

--- src/main/java/com/azerty/quizgame/config/SecurityConfiguration.java
+++ src/main/java/com/azerty/quizgame/config/SecurityConfiguration.java
@@ -14,17 +14,19 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfiguration {
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final AuthenticationProvider authenticationProvider;
+public class SecurityConfiguration
+{
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	private final AuthenticationProvider authenticationProvider;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeHttpRequests()
-                .requestMatchers("/api/**").permitAll().anyRequest().authenticated().and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-        return http.build();
-    }
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+	{
+		http.csrf().disable().authorizeHttpRequests().requestMatchers("/api/auth/authenticate").permitAll()
+				.requestMatchers("/swagger-ui/index.html").permitAll().anyRequest().authenticated().and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.authenticationProvider(authenticationProvider)
+				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+		return http.build();
+	}
 }
